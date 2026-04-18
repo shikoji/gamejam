0adb7c669d07ee957b5b13c71105d840217ff77c
Shikoji <robsongomes@acad.ifma.edu.br>
2026-04-18 17:10:51 -0300

    Add files via upload

---
+++ gamemanger.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var player: PackedScene
+@export var book: PackedScene
+
+var player_SPEED = 75.0
+var playerDefault_SPEED = 75.0
+
+var playerDefault_JUMP_VELOCITY = -200 
+var player_JUMP_VELOCITY = -200.0
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
